76b85854fe3462622554bd6824a9adbf442fdf00
Madi Rei <madison.nicole.rei@gmail.com>
2025-12-04 11:37:58 -0800

    adds changes made during class (begins waves 3&4 - still incomplete)

---
+++ src/index.js
@@ -0,0 +1,131 @@
+
+//________________________________________________________________________________
+// wave 2
+
+
+
+const increaseTemp = () => {
+  let tempValue = document.getElementById('tempValue');
+  const tempContainer = document.createElement('span');
+  tempValue.textContent = tempValue;
+  tempContainer.appendChild(tempValue);
+  tempValue += 1;
+};
+
+const registerEventHandlers = () => {
+  const increaseTempControlButton = document.getElementById('increaseTempControl');
+  increaseTempControlButton.addEventListener('click', increaseTemp);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//________________________________________________________________________________
+// wave 3
+const cityNameInput = document.getElementById('cityNameInput');
+const cityNameResetButton = document.getElementById('cityNameReset');
+
+cityNameInput.addEventListener('input', )
+
+
+
+
+
+
+
+
+
+
+//________________________________________________________________________________
+// wave 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//________________________________________________________________________________
+// wave 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//________________________________________________________________________________
+// wave 6
+
